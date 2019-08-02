b45a09e70e5b2be18e4f253886077d0cfba0b2e1
Venkataramana Radhakrishnan <venkat@zendoc.com>
2019-08-02 21:56:47 +0200

    updated setup and added requirements

--- src/setup.py
+++ src/setup.py
@@ -4,9 +4,9 @@ from distutils.core import setup
 setup(name='custom_train_and_clustering',
       author='Venkataramana Radhakrishnan',
       author_email='venkyr91193@gmail.com',
-      description='To train your custom word2vec for work specific tasks',   
+      description='To train your custom word2vec for work specific tasks in many languages',   
       version='0.0.1',
       packages = ['train_and_cluster'],
-      py_modules=[''],
-      install_requires=[''],
+      py_modules=['cluster','languages','load_train_cluster','train'],
+      install_requires=['spacy','sklearn','nltk','gensim','numpy'],
       )
