2883a40c8535e852af9aac9109d202e7ff895056
Venkataramana Radhakrishnan <venkyr91193@gmail.com>
2020-05-12 17:40:39 +0200

    refactor(tests): adapt test cases

--- src/tests/full_test.py
+++ src/tests/full_test.py
@@ -1,11 +1,11 @@
 import sys, os
 sys.path.insert(0,os.path.join(os.path.dirname(os.path.dirname(__file__))))
-from train_and_cluster import SentenceClustering
+from train_and_cluster import TextClustering
 
 def test_whole_pipe():
   sentences = ['Today is a good day.','I see an aeroplane in the sky.','Alex and Mic are best friends.']
-  clusters = SentenceClustering(sentences,os.path.join(os.path.dirname(os.path.dirname(__file__))),'English',2,'nltk')
-  clusters = SentenceClustering(sentences,os.path.join(os.path.dirname(os.path.dirname(__file__))),'English',2,'sklearn')
+  clusters = TextClustering(sentences,os.path.join(os.path.dirname(os.path.dirname(__file__))),'en',2,'nltk')
+  clusters = TextClustering(sentences,os.path.join(os.path.dirname(os.path.dirname(__file__))),'en',2,'sklearn')
 
 if __name__ == "__main__":
   test_whole_pipe()
